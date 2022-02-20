26c9a9c5ed52581470dd33037b2b4ebc9fa79196
severopol <58575695+severopol@users.noreply.github.com>
2022-02-20 22:25:04 +0000

    Fixed core functionality

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -6,10 +8,11 @@ import java.util.Set;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
-		String wordToGuess = "sours";
+		String wordToGuess = generateWord();
 		String outputString = "";
+		String outputPrint = "";
 		StringBuilder output = new StringBuilder("");
 		int guessCount = 0;
 		ArrayList<Character> letters = new ArrayList<>();
@@ -19,7 +22,7 @@ public class App {
 			output = new StringBuilder("");
 
 			// getting the user input - user guess
-			System.out.println("Please enter a 5 letter word");
+			System.out.println("Please enter a 5 letter word:\n");
 			scanner = new Scanner(System.in);
 			String input = scanner.nextLine();
 
@@ -45,15 +48,13 @@ public class App {
 				if (output.length() == i) {
 					output.append("*");
 				}
-
 			}
 
-			System.out.println(output);
-
 			outputString = output.toString();
-
+			outputPrint = output.toString().toUpperCase();
+			System.out.println(outputPrint);
+			
 			// REMOVE DUPLICATES FROM ARRAY LIST
-		
 			Iterator<Character> it = letters.iterator();
 			ArrayList<Character> reducedList = new ArrayList<>();
 			reducedList.addAll(letters);
@@ -77,17 +78,36 @@ public class App {
 			reducedList.addAll(set);
 
 			if (!reducedList.isEmpty()) {
-					System.out.println("Correct letter, wrong position: " + reducedList);
+					System.out.println("Correct letter, wrong position: " + reducedList + "\n");
+				} else {
+					System.out.println("\n");
 				}
-
-			outputString = output.toString();
-			guessCount++;
 			
+			guessCount++;
 			
 		} while (!outputString.equals(wordToGuess));
 		
 			System.out.println("You guessed correctly, well done!");
 			System.out.println("You made " + guessCount + " guesses");
 			scanner.close();
+	}
+
+	private static String generateWord() throws FileNotFoundException {
+		
+		File file = new File("configs/wordList.txt");
+		Scanner scanner = new Scanner(file);
+		int rand = (int) ((Math.random() * (5757 - 1)) + 1);
+		int count = 1;
+		
+		while (scanner.hasNextLine() && count < rand) {
+			String l = scanner.nextLine();
+			count++;
+		}
+		
+		String toReturn = scanner.nextLine().toLowerCase();
+		scanner.close();
+		
+		return toReturn;
+		
 	}
 }
