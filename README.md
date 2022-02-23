a3ea1b000512c5c602dd4902284d4ee5f4395a8f
jessl <jessl@DESKTOP-VRUJQVV>
2022-02-23 00:07:32 +0000

    requests number length, validates length of word, some change to sysout

--- README.md
+++ README.md
@@ -3,19 +3,27 @@ Basic wordle project
 
 Todo list:
 - [ ] Validate user input is correct format
-- [ ] Validate user input is correct length 
+- [x] Validate user input is correct length 
 - [ ] Validate user input is a dictionary word
 - [x] Randomise wordToGuess using dictionary csv file
 - [ ] Add option for varying length wordToGuess
 - [ ] Format console output
 - [ ] Print out rules at start
 - [ ] Refactor methods
-
+- [ ] Maximum guess count?
+- [ ] Option for new game  
+- [ ] Caching game history 
+- [ ] Capitalise letters in array list
+- [ ] try/catch for number length request in case user inputs words etc. 
+- [ ] edit word trimmer to allow for requested lengths 
+- [ ] need to change random tool to NOT be hardcoded to number of lines in file 
 
 
 Bugs:
 - [ ] Asterixes returned is length of input - limit to size of wordToGuess?
+- [ ] Random line generator hardcoded to exact length of lines in the file 
 
 
 
-
+Notes:
+- WordlistTrimmer currently hardcoded at 5 letters
--- src/com/application/App.java
+++ src/com/application/App.java
@@ -1,4 +1,5 @@
 package com.application;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -18,14 +19,53 @@ public class App {
 		int guessCount = 0;
 		ArrayList<Character> letters = new ArrayList<>();
 		Scanner scanner;
-		
+		String input = "";
+		boolean isWordCorrectLength = false;
+		int characterLength;
+		scanner = new Scanner(System.in);
+		String chosenLength = "";
+		boolean isValidNumber = false;
+		int num = 0;
+		System.out.println("Enter your desired word length between 4 and 10");
+
+		while (!isValidNumber) {
+
+			chosenLength = scanner.nextLine();
+			num = Integer.parseInt(chosenLength);
+
+			if (num >= 4 && num <= 10) {
+
+				isValidNumber = true;
+
+			} else {
+				System.out.println("Length not between 4 and 10. Please enter again.");
+			}
+		}
+
 		do {
 			output = new StringBuilder("");
 
-			// getting the user input - user guess
-			System.out.println("Please enter a 5 letter word:\n");
-			scanner = new Scanner(System.in);
-			String input = scanner.nextLine();
+			// getting the user input - user guess OR asking user to guess again
+			if (guessCount > 0)
+				System.out.println("Guess again:\n");
+			else {
+				System.out.println("Please enter a " + num + " letter word.\n");
+			}
+
+			// while loop for validating length of input string
+			while (!isWordCorrectLength) {
+
+				input = scanner.nextLine();
+
+				if (input.length() == num) {
+
+					isWordCorrectLength = true;
+
+				} else {
+
+					System.out.println("Wrong word length. Please enter a " + num + " letter word.\n");
+				}
+			}
 
 			input = input.toLowerCase();
 
@@ -34,10 +74,10 @@ public class App {
 				char wordLetter = input.charAt(i);
 
 				for (int j = 0; j < wordToGuess.length(); j++) {
-					
+
 					if (wordLetter == wordToGuess.charAt(j)) {
 						if (i == j) {
-							
+
 							output.append(wordToGuess.charAt(i));
 							break;
 						} else {
@@ -54,61 +94,63 @@ public class App {
 			outputString = output.toString();
 			outputPrint = output.toString().toUpperCase();
 			System.out.println(outputPrint);
-			
+
 			// REMOVE DUPLICATES FROM ARRAY LIST
 			Iterator<Character> it = letters.iterator();
 			ArrayList<Character> reducedList = new ArrayList<>();
 			reducedList.addAll(letters);
 
 			while (it.hasNext()) {
-				
+
 				it.next();
-				
+
 				for (int i = 0; i < letters.size(); i++) {
 
 					if (outputString.contains(letters.get(i) + "")) {
-						
+
 						reducedList.remove(letters.get(i));
 					}
 				}
-					it.remove();
+				it.remove();
 			}
-			
+
 			Set<Character> set = new LinkedHashSet<Character>(reducedList);
 			reducedList.clear();
 			reducedList.addAll(set);
 
 			if (!reducedList.isEmpty()) {
-					System.out.println("Correct letter, wrong position: " + reducedList + "\n");
-				} else {
-					System.out.println("\n");
-				}
-			
+				System.out.println("Correct letter, wrong position: " + reducedList + "\n");
+			} else {
+				System.out.println("\n");
+			}
+
 			guessCount++;
-			
+
+			isWordCorrectLength = false;
+
 		} while (!outputString.equals(wordToGuess));
-		
-			System.out.println("You guessed correctly, well done!");
-			System.out.println("You made " + guessCount + " guesses");
-			scanner.close();
+
+		System.out.println("You guessed correctly, well done!");
+		System.out.println("You made " + guessCount + " guesses");
+		scanner.close();
 	}
 
 	private static String generateWord() throws FileNotFoundException {
-		
+
 		File file = new File("configs/FiveLetterFrequentWords.txt");
 		Scanner scanner = new Scanner(file);
 		int rand = (int) ((Math.random() * (686 - 1)) + 1);
 		int count = 1;
-		
+
 		while (scanner.hasNextLine() && count < rand) {
 			String l = scanner.nextLine();
 			count++;
 		}
-		
+
 		String toReturn = scanner.nextLine().toLowerCase();
 		scanner.close();
-		
+
 		return toReturn;
-		
+
 	}
 }
